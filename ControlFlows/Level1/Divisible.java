e1e902725d703431abbede3466cdcfa9fd914bd7
Aryan Gupta <120731042+thearyangupta@users.noreply.github.com>
2025-07-01 08:46:51 +0530

    Delete ControlFlows directory

--- ControlFlows/Level1/Divisible.java
+++
@@ -1,8 +0,0 @@
-public class Divisible {
-    static void checkDivisible(int number) {
-        System.out.println(number % 5 == 0);
-    }
-    public static void main(String[] args) {
-        checkDivisible(25);
-    }
-}
